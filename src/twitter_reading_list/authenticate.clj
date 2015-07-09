4df22dabe8496c9dcb2a62f0e6e5b8197e34ffc8
David Genn <davidgenn@gmail.com>
2015-07-09 21:10:39 +0100

    Started to sort out twitter and authentication: \n * Tried to get pin based auth working

--- src/twitter_reading_list/authenticate.clj
+++ src/twitter_reading_list/authenticate.clj
@@ -4,10 +4,10 @@
     [nomad :refer [defconfig]]
     [clojure.java.io :as io]))
 
-(defconfig config (io/resource "resources/config.edn"))
+(defconfig config (io/resource "config.edn"))
 
 ;; Create a Consumer to access Twitter.
-(def consumer (oauth/make-consumer (:twitter-token (config))
+(def consumer  (oauth/make-consumer (:twitter-token (config))
                                    (:twitter-secret (config))
                                    "https://api.twitter.com/oauth/request_token"
                                    "https://api.twitter.com/oauth/access_token"
@@ -21,8 +21,10 @@
 ;; to choose the level of access to grant the application and will
 ;; then be redirected to the callback URI provided with the
 ;; request-token.
-(oauth/user-approval-uri consumer
-                         (:oauth_token request-token))
+(defn approval-url []
+  (oauth/user-approval-uri consumer
+                           (:oauth_token request-token)))
+
 
 ;; Assuming the User has approved the request token, trade it for an access token.
 ;; The access token will then be used when accessing protected resources for the User.
@@ -30,17 +32,18 @@
 ;; If the OAuth Service Provider provides a verifier, it should be included in the
 ;; request for the access token.  See [Section 6.2.3](http://oauth.net/core/1.0a#rfc.section.6.2.3) of the OAuth specification
 ;; for more information.
-(def access-token-response (oauth/access-token consumer
-                                               request-token
-                                               "7276539"))  ;; PIN
+(defn access-token-response [pin] (oauth/access-token consumer
+                                                request-token
+                                               pin))  ;; PIN
 
 ;; Each request to a protected resource must be signed individually.  The
 ;; credentials are returned as a map of all OAuth parameters that must be
 ;; included with the request as either query parameters or in an
 ;; Authorization HTTP header.
-(defn credentials [url] (oauth/credentials consumer
-                                    (:oauth_token access-token-response)
-                                    (:oauth_token_secret access-token-response)
-                                    :GET
-                                    url
-                                    ))
+(defn credentials [url pin]
+  (let [token-response (access-token-response pin)]
+    (oauth/credentials consumer
+                       (:oauth_token token-response)
+                       (:oauth_token_secret token-response)
+                       :GET
+                       url)))
--- src/twitter_reading_list/twitter.clj
+++ src/twitter_reading_list/twitter.clj
@@ -3,8 +3,7 @@
     [clj-http.client :as http]
     [twitter-reading-list.authenticate :as authenticate]))
 
-(defn users-timeline
-  (let [credentials (authenticate/credentials "https://api.twitter.com/1.1/statuses/home_timeline.json")]
+(defn users-timeline [pin]
+  (let [credentials (authenticate/credentials "https://api.twitter.com/1.1/statuses/home_timeline.json" pin)]
     (http/get "https://api.twitter.com/1.1/statuses/home_timeline.json"
-              {:query-params credentials}))
-  )
+              {:query-params credentials})))
