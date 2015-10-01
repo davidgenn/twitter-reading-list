bb27e1542cb441887eda44cf2d1473dba139e668
David Genn <davidgenn@gmail.com>
2015-10-01 20:43:32 +0100

    Added email password to config.edn

--- src/twitter_reading_list/email.clj
+++ src/twitter_reading_list/email.clj
@@ -1,11 +1,18 @@
 (ns twitter-reading-list.email
-  (:require [postal.core :as postal]))
+  (:require 
+    [postal.core :as postal]
+    [nomad :refer [defconfig]]
+    [clojure.java.io :as io]))
+
+(defconfig config (io/resource "config.edn"))
+
+
 
 ;; requires permission for 'less secure' apps to be turned on
 (defn send-email []
   (postal.send-message {:host "smtp.gmail.com"
                             :user "davidgenn"
-                            :pass password
+                            :pass (:gmail-password (config))
                             :tls true}
                            {:from "davidgenn@gmail.com"
                             :to "davidgenn@gmail.com"
